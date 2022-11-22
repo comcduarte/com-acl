205feee132ba3e6b761bd95f0def0c286116915f
Duartec <Duartec@IT-WKS017.MidNet.CityOfMiddletown.com>
2022-11-22 15:41:21 -0500

    FIX: Moved AclAwareTrait to AclModule

---
+++ src/Traits/AclAwareTrait.php
@@ -0,0 +1,32 @@
+<?php
+namespace Acl\Traits;
+
+trait AclAwareTrait
+{
+    protected $acl_service = NULL;
+    
+    public function setAclService($acl_service)
+    {
+        $this->acl_service = $acl_service;
+        return $this;
+    }
+    
+    public function getAclService()
+    {
+        return $this->acl_service;
+    }
+    
+    public function isAllowed($roles, $resource, $privilege)
+    {
+        if (is_null($roles)) {
+            return FALSE;
+        }
+        
+        foreach ($roles as $role_object) {
+            if ($this->getAclService()->isAllowed($role_object['ROLENAME'], $resource, $privilege)) {
+                return TRUE;
+            }
+        }
+        return FALSE;
+    }
+}
